6b67eb7d825428b3979b3ed64f23b3350470879e
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-07-19 22:17:39 +0900

    Brushup MoneyTest.java

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -2,14 +2,19 @@ package money;
 
 public class Dollar {
 
-	int amount;
+	private int amount;
 
     Dollar(int amount) {
     	this.amount = amount;
     }
 
-    void times(int multiplier) {
-    	amount *= multiplier;
+    Dollar times(int multiplier) {
+    	return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+    	Dollar dollar = (Dollar) object;
+    	return amount == dollar.amount;
     }
 
 }
--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -9,8 +9,14 @@ public class MoneyTest {
 	@Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
+
+	@Test
+    public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+//		assertTrue(new Dollar(5).equals(new Dollar(6)));
+	}
 
 }
