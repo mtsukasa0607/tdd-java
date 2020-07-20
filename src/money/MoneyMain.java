c6fc26142fa701ba0a065b32a068058c9f37a794
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-07-20 18:01:12 +0900

    Fixed Franc.java

--- src/money/MoneyMain.java
+++ src/money/MoneyMain.java
@@ -3,7 +3,6 @@ package money;
 public class MoneyMain {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		Dollar five = new Dollar(5);
 		 five.times(2);
 		 System.out.println(five.getAmout());
