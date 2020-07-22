461351905658a7156c9f8b19c2fc059ef4097233
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-07-22 21:54:49 +0900

    Complete Lesson 12

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -18,19 +18,21 @@ public class MoneyTest {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
-	}
-
-	@Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
 	@Test
     public void testCurrency() {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
+	}
+
+	@Test
+	public void testSimpleAddition() {
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(10), reduced);
 	}
 
 }
