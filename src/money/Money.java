510ac17e77ad42ea8fcb361c428f0141bfbbe86c
mtsukasa0607 <TsukasaMatsushita0607>
2020-07-23 05:42:31 +0900

    Complete Lesson 14

--- src/money/Money.java
+++ src/money/Money.java
@@ -38,8 +38,9 @@ class Money implements Expression {
 		return new Money(amount, "CHF");
 	}
 
-	public Money reduce(String to) {
-		return this;
+	public Money reduce(Bank bank, String to) {
+		int rate = bank.rate(currency, to);
+		return new Money(amount / rate, to);
 	}
 
 }
